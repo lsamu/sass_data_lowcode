e7ffc10e7f1bad8bd678c670c45085e216058e0d
lauxinyi <lauxinyi@gmail.com>
2023-07-13 15:54:29 +0800

    Updates

---
+++ assets/demo-3a905d33.js
@@ -0,0 +1,1 @@
+import{d as t,a as n,h as a,o as s}from"./index-3b3f342c.js";const m=t({__name:"demo",setup(c){const o={};return(_,p)=>{const e=n("box-table");return s(),a(e,{option:o})}}});export{m as default};
