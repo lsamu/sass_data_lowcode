1420f4bc7aa0c36edd0b8fcb75e29139ec4fa04e
lauxinyi <lauxinyi@gmail.com>
2023-07-27 14:16:30 +0800

    Updates

--- assets/index-67b813ae.js
+++
@@ -1,1 +0,0 @@
-import{d as o,r as a,a as t,o as n,h as r}from"./index-80b90da5.js";const i=o({__name:"index",setup(s){return a({}),(c,_)=>{const e=t("box-validate");return n(),r(e)}}});export{i as default};
