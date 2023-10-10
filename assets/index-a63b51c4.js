346cdc0c57d7a282a6463cfaa6295ae4b02722af
lauxinyi <lauxinyi@gmail.com>
2023-10-10 18:09:44 +0800

    Updates

---
+++ assets/index-a63b51c4.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index.vue_vue_type_style_index_0_lang-8527f177.js";import{d as o,l as a,o as n}from"./index-4b946c50.js";const m=o({__name:"index",setup(r){return(t,_)=>(n(),a(e))}});export{m as default};
---
+++ assets/system-572d2200.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as t}from"./index-4b946c50.js";const n={};function o(r,s){return t(),c("div",null," 系统管理 ")}const a=e(n,[["render",o]]);export{a as default};
